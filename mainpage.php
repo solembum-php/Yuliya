e9c59efbc4269aa534111d538da0fcbbbfdf764d
Vasyl <admin@gmail.com>
2019-08-30 20:07:23 +0300

    create file mainpage.php

---
+++ mainpage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Yuliya's Site</title>
+    </head>
+    <body>
+	<header>
+	    <h1>Yuliya's Site</h1>
+	</header>
+	<main>
+	    <h2>Hi, all cat lovers!</h2>
+	</main>
+	<footer>Powered by solembum 2019</footer>
+	<?php
+	?>
+    </body>
+</html>
