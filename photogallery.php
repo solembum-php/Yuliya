e1a93c6bb7d85305f04648c1baef93c4c299c9d1
Vasyl <admin@gmail.com>
2019-08-30 20:15:32 +0300

    create file photogallery.php

---
+++ photogallery.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Yuliya's Photogallery</title>
+    </head>
+    <body>
+	<header>
+	    <h1>Yuliya's Photogallery</h1>
+	</header>
+	
+	<?php
+	// put your code here
+	?>
+    </body>
+</html>
