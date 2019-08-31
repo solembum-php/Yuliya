6dbb47a762d8233b9e19ff36c27d6055dea8a2c8
solembum-php <solembum@i.ua>
2019-08-31 13:55:54 +0300

    correct editor in editmainpage.php

--- editmainpage.php
+++ editmainpage.php
@@ -6,22 +6,14 @@
         <script src="../ckeditor.js"></script>
     </head>
     <body>
-        <header>
-            <h1>Yuliya's Site</h1>
-        </header>
-        <main>
-            <h2>Hi, all cat lovers!</h2>
-            <form>
-                <textarea name="editor1" id="editor1" rows="10" cols="80">
+        <form>
+            <textarea name="editor1" id="editor1" rows="10" cols="80">
                     Here's some information on cats I'd like to share with you.
-                </textarea>
-                <script>
-                    CKEDITOR.replace('editor1');
-                </script>
-            </form>
-        </main>
-        <?php
-        ?>
+            </textarea>
+            <script>
+                CKEDITOR.replace('editor1');
+            </script>
+        </form>
     </body>
 </html>
 
