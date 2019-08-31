aa48edd553a941228e569bb5781eaaf0dc852cc7
solembum-php <solembum@i.ua>
2019-08-31 13:45:44 +0300

    add to adminpage.php

--- adminpage.php
+++ adminpage.php
@@ -8,11 +8,11 @@
 	<form method="post" name="admin_page" action="admin_correct.php">
             <label>
                 Login:
-                <input type="text" name="login" value="admin"/>
+                <input type="text" name="login" value="admin" readonly/>
             </label>
 	    <label>
                 Password:
-                <input type="password" name="pass" value="admin"/>
+                <input type="password" name="pass" value="admin" readonly/>
             </label>
             <input type="submit" value="log in"/>
         </form>
