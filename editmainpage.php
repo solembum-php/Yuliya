64adb2036d8a594f4054d22cfb5125a0ac7e2b16
solembum-php <solembum@i.ua>
2019-08-31 11:54:49 +0300

    add to editmainpage.php

--- editmainpage.php
+++ editmainpage.php
@@ -1,4 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>main page</title>
+    </head>
+    <body>
+	<header>
+	    <h1>Yuliya's Site</h1>
+	</header>
+	<main>
+            <h2>Hi, all cat lovers!</h2>
+            <textarea></textarea>
+	</main>
+	<?php
+	?>
+    </body>
+</html>
 
 
 
