0982aef61aed332c48e2c0cb1748a6b11ad07918
solembum-php <solembum@i.ua>
2019-09-01 19:51:24 +0300

    add to mainpage.css and photogallery.php

--- photogallery.php
+++ photogallery.php
@@ -8,18 +8,20 @@ $pictures = getAllPictures();
     <head>
         <meta charset="UTF-8">
         <title>Yuliya's Photogallery</title>
+        <link href="CSS/mainpage.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-	<header>
-	    <h1>Yuliya's Photogallery</h1>
-	</header>
-	
-        <?php foreach ($pictures as $key => $picture) : ?>
-            <div id="pictures">
-                <?php if ($key !== 0 && $key !== 1) : ?>
-                <a><img src="photos/<?= $picture ?>" alt="photo" width="250"/></a>
-                <?php endif; ?>
-            </div>
-        <?php endforeach; ?>
+        <header>
+            <h1>Yuliya's Photogallery</h1>
+        </header>
+        <main>
+            <?php foreach ($pictures as $key => $picture) : ?>
+                <div id="pictures">
+                    <?php if ($key !== 0 && $key !== 1) : ?>
+                        <a><img src="photos/<?= $picture ?>" alt="photo" width="200"/></a>
+                    <?php endif; ?>
+                </div>
+            <?php endforeach; ?>
+        </main>
     </body>
 </html>
