1ee5be31437225bb3c4aa67372ac24600e00f937
solembum-php <solembum@i.ua>
2019-09-02 16:03:07 +0300

    create file functionstextedit.php and add photos/cat6.jpg and photos/cat8.jpg to folder photos

---
+++ functionstextedit.php
@@ -0,0 +1,36 @@
+<?php
+
+function getRequestText() {
+    $new_text = $_POST['new_text'];
+    $text = array(
+            'new_text' => $new_text,
+        );
+    return $text;
+}
+
+function getText() {
+    if (!file_exists(MAINPAGE_TEXT)) {
+        return false;
+    } else {
+        $content = file_get_contents(MAINPAGE_TEXT);
+        if (!$content) {
+            return false;
+        } else {
+            $texts = json_decode($content, true);
+            return $texts;
+        }
+    }
+}
+
+function saveText($texts) {
+    $content = json_encode($texts);
+    $res = file_put_contents(MAINPAGE_TEXT, $content);
+    return (bool) $res;
+}
+
+function editText($text) {
+    $texts = getText();
+    $texts[] = $text;
+    saveText($texts);
+}
+
